43503d589704c9230a36d249426cb186a9f1b21d
xm1k3 <73166077+xm1k3@users.noreply.github.com>
2021-06-10 13:42:26 +0200

    fleex init added

---
+++ cmd/config.go
@@ -0,0 +1,80 @@
+package cmd
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// initCmd represents the init command
+var configCmd = &cobra.Command{
+	Use:   "config",
+	Short: "fleex config setup",
+	Long:  "fleex init project",
+}
+
+var configInit = &cobra.Command{
+	Use:   "init",
+	Short: "fleex init project",
+	Long:  "fleex init project",
+	Run: func(cmd *cobra.Command, args []string) {
+		configPath, _ := rootCmd.PersistentFlags().GetString("config")
+		fmt.Println(configPath)
+
+		viper.SetConfigType("yaml")
+		viper.SetDefault("provider", "linode")
+		viper.SetDefault("linode-image", "{YOUR LINODE IMAGE}")
+		viper.SetDefault("linode-region", "{YOUR REGION}")
+		viper.SetDefault("linode-token", "{YOUR TOKEN}")
+		err := viper.SafeWriteConfigAs(configPath)
+		fmt.Println(err)
+	},
+}
+
+var configGet = &cobra.Command{
+	Use:   "get",
+	Short: "fleex get data from config file",
+	Long:  "fleex get data from config file",
+	Run: func(cmd *cobra.Command, args []string) {
+		fieldFlag, _ := cmd.Flags().GetString("field")
+		viper.SetConfigType("yaml")
+		viper.ReadInConfig()
+
+		if strings.Contains(fieldFlag, ",") {
+			fields := strings.Split(fieldFlag, ",")
+			for _, singleField := range fields {
+				field := viper.Get(singleField)
+				fmt.Println("-", singleField, ":", field)
+			}
+		} else {
+			fmt.Println("-", fieldFlag, ":", viper.Get(fieldFlag))
+		}
+	},
+}
+
+var configSet = &cobra.Command{
+	Use:   "set",
+	Short: "fleex set data in config file",
+	Long:  "fleex set data in config file",
+	Run: func(cmd *cobra.Command, args []string) {
+		// configPath, _ := rootCmd.PersistentFlags().GetString("config")
+		key, _ := cmd.Flags().GetString("key")
+		value, _ := cmd.Flags().GetString("value")
+		viper.SetConfigType("yaml")
+		viper.Set(key, value)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(configCmd)
+	configCmd.AddCommand(configInit)
+	configCmd.AddCommand(configGet)
+	configCmd.AddCommand(configSet)
+
+	configGet.Flags().StringP("field", "f", "", "field to retrieve, comma separated")
+	configSet.Flags().StringP("key", "k", "", "key")
+	configSet.Flags().StringP("value", "v", "", "value")
+
+}
--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/sw33tLie/fleex
 go 1.16
 
 require (
-	github.com/mitchellh/go-homedir v1.1.0 // indirect
-	github.com/spf13/cobra v1.1.3 // indirect
-	github.com/spf13/viper v1.7.1 // indirect
-	github.com/tidwall/gjson v1.8.0 // indirect
-	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/spf13/cobra v1.1.3
+	github.com/spf13/viper v1.7.1
+	github.com/tidwall/gjson v1.8.0
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a
 )
