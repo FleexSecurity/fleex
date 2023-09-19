1a25d5f66b86152d15aa87f55002f91cc85f108a
xm1k3 <73166077+xm1k3@users.noreply.github.com>
2023-09-19 11:35:58 +0200

    added custom vps configs

--- pkg/models/config.go
+++ pkg/models/config.go
@@ -2,19 +2,31 @@ package models
 
 type Config struct {
 	Providers map[string]Provider `json:"providers"`
+	CustomVMs []CustomVM          `json:"custom_vms"`
 	SSHKeys   SSHKeys             `json:"ssh_keys"`
 	Settings  Settings            `json:"settings"`
 }
 
 type Provider struct {
-	Token    string   `json:"token"`
-	Region   string   `json:"region"`
-	Size     string   `json:"size"`
-	Image    string   `json:"image"`
-	Port     int      `json:"port"`
-	Username string   `json:"username"`
-	Password string   `json:"password"`
+	Token    string   `json:"token,omitempty"`
+	Region   string   `json:"region,omitempty"`
+	Size     string   `json:"size,omitempty"`
+	Image    string   `json:"image,omitempty"`
+	Port     int      `json:"port,omitempty"`
+	Username string   `json:"username,omitempty"`
+	Password string   `json:"password,omitempty"`
 	Tags     []string `json:"tags,omitempty"`
+}
+
+type CustomVM struct {
+	Provider   string   `json:"provider"`
+	InstanceID string   `json:"instance_id"`
+	PublicIP   string   `json:"public_ip"`
+	SSHPort    int      `json:"ssh_port"`
+	Username   string   `json:"username"`
+	Password   string   `json:"password"`
+	KeyPath    string   `json:"key_path"`
+	Tags       []string `json:"tags"`
 }
 
 type SSHKeys struct {
